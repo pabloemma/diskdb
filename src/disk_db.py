75d709cac1876ad81c33b3feb8cb20b4aeb9a26a
andi klein <pabloemma@gmail.com>
2025-08-06 11:33:02 -0600

    now working create table

--- src/disk_db.py
+++ src/disk_db.py
@@ -75,11 +75,12 @@ class MY_DB(object):
             ISOLATION_LEVEL_AUTOCOMMIT = True #contary to AI; needs to be set to True
             MyConn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
             MyConn.autocommit = True
-
             self.MyCurs = MyCurs = MyConn.cursor()
 
             self.MyCurs.execute(f"CREATE DATABASE {self.db_name};")
         
+        self.MyConn = MyConn
+
         # check if database exists:
 
         stat =  '''select exists(SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower(\''''+self.db_name+'\'))''';
@@ -108,8 +109,10 @@ class MY_DB(object):
     def column_command(self,columns):
         mycommand = []
         sql_command = ''
-        for k in range(len(columns)):
-            mycommand.append(columns[k][0]+' '+columns[k][1]+' '+columns[k][2])
+        print(len(columns))
+        for k in range(0,len(columns)-1,3):
+            #mycommand.append(columns[k][0]+' '+columns[k][1]+' '+columns[k][2])
+            mycommand.append(columns[k]+' '+columns[k+1]+' '+columns[k+2])
 
  #       for k in range(len(mycommand)-1):
  #           sql_command =sql_command + mycommand[k] +','
@@ -127,19 +130,32 @@ class MY_DB(object):
         is of the form [variable name,type,constraint]"""
         sql_command_new = ''
         sql_command = ''
-        temp = self.column_command(columns)
+        if(table_name == 'disk_table'):
+            temp = self.column_command(self.CD.disk_table)
+        elif(table_name == 'directory_table'):
+            temp = self.column_command(self.CD.directory_table)
+        elif(table_name == 'file_table'):
+            temp = self.column_command(self.CD.file_table)
+        else:
+            logger.error("table %s not known" % table_name)
+           
+             
+
         
  #       sql_command_new = 'CREATE TABLE IF NOT EXISTS '+table_name+'  ('+self.column_command(columns)+' )'
         for k in range(len(temp)-1):
             sql_command =sql_command + temp[k] +','
-        sql_command_new ='CREATE TABLE IF NOT EXISTS '+table_name+' ( '+sql_command+temp[k+1]+' )'
-
+        sql_command_new ='CREATE TABLE IF NOT EXISTS '+table_name+' ( '+sql_command+temp[k+1]+' );'
         self.MyCurs.execute(sql_command_new)
+
+        self.MyConn.commit()
+        return
+    
 
 
     def create_columns(self,conf_column = None):
         """this takes the json columns and form it again into lists for the create Table"""
-        conf_column = self.CD.disk_table
+        #conf_column = self.CD.disk_table
         temp_column = []
 
         a = []
@@ -196,23 +212,15 @@ if __name__ == "__main__":
     db_name = 'disk'
     test = MY_DB(db_name = db_name,db_user = 'klein',db_pwd = '?Pa!blo?solveig',db_host = '192.168.2.230' , db_system = 'PSQL')
     test.connect_db()
-    test.create_columns()
+    #test.create_columns()
     #test.create_db(db_name = 'disk',db_user = 'klein')
 
-    columns=[['disk_id',
-              'SERIAL','PRIMARY KEY'],
-              ['name','varchar[40]','NOT NULL'],
-              ['size','NUMERIC','NOT NULL'],
-              ['used ','NUMERIC','NOT NULL'],
-              ['available','NUMERIC','NOT NULL'],
-              ['file_system','varchar[20]','NOT NULL'],
-              ['topdir','TEXT','NOT NULL'],
-              ['notes','TEXT']
-              ]
     
     #columns1=[['disk_test2','varchar[30]',''],['name2','varchar[40]','NOT NULL'],['size2','NUMERIC','']]
     
-    #test.create_table('disk_table',columns=columns)
+    test.create_table('disk_table')
+    test.create_table('directory_table')
+    test.create_table('file_table')
     #test.add_columns(table_name='disk_table',columns=columns1)
     #test.delete_db(db_name = 'disk')
 
