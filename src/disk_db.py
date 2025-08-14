00207fff0f013f472dcbacd4d191176f67655e4f
andi klein <pabloemma@gmail.com>
2025-08-14 15:52:49 -0600

    took care of the '\r' problem

--- src/disk_db.py
+++ src/disk_db.py
@@ -296,12 +296,12 @@ class MY_DB(object):
                 for afile in os.listdir(adir):
                     apath = os.path.join(adir,afile)
                     if os.path.isfile(apath):
-                        myfile_list.append(afile)
+                        myfile_list.append(afile.strip('\r')) #for screwed uip entries
                         myfile_list.append(os.path.getsize(apath))
                 if (self.CD.log_level=='DEBUG'):
                     for k in range(0,len(myfile_list)-1,2):
                        
-                       print('file        {0:30s}  filesize {1:15d}'.format(myfile_list[k].strip('\r'),myfile_list[k+1]))
+                       print('file        {0:30s}  filesize {1:15d}'.format(myfile_list[k],myfile_list[k+1]))
 
                 if (self.CD.log_level=='DEBUG'):
  
