cb880245d10fe593427be5e2d1b6655f5d19a54f
andi klein <pabloemma@gmail.com>
2025-08-17 16:42:45 -0600

    back to full directory depth

--- src/disk_db.py
+++ src/disk_db.py
@@ -261,8 +261,8 @@ class MY_DB(object):
         """finds all directories and file to max_depth"""
 
         #first get max level
-        #max_depth = self.DI.get_max_directory_level(path)
-        max_depth = 2
+        max_depth = self.DI.get_max_directory_level(path)
+        #max_depth = 2
 
         
         #max_depth = self.CD.max_depth
